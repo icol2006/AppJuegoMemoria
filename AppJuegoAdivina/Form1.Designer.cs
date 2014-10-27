103ff9c3b28ad0cf37de1bd78ffc03a48542f1e1
Maykol <icol2006@gmail.com>
2014-10-26 18:23:46 -0600

    v1

---
+++ AppJuegoAdivina/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace AppJuegoAdivina
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblOpcion1 = new System.Windows.Forms.Label();
+            this.lblOpcion2 = new System.Windows.Forms.Label();
+            this.lblOpcion3 = new System.Windows.Forms.Label();
+            this.btnJugar = new System.Windows.Forms.Button();
+            this.lbl = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblSeleccionar = new System.Windows.Forms.Label();
+            this.lblNivel = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.empezarJuegoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblOpcion1
+            // 
+            this.lblOpcion1.AutoSize = true;
+            this.lblOpcion1.BackColor = System.Drawing.Color.Black;
+            this.lblOpcion1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblOpcion1.Location = new System.Drawing.Point(22, 103);
+            this.lblOpcion1.Name = "lblOpcion1";
+            this.lblOpcion1.Size = new System.Drawing.Size(22, 13);
+            this.lblOpcion1.TabIndex = 0;
+            this.lblOpcion1.Tag = "  ";
+            this.lblOpcion1.Text = "     ";
+            this.lblOpcion1.Click += new System.EventHandler(this.lblOpcion1_Click);
+            // 
+            // lblOpcion2
+            // 
+            this.lblOpcion2.AutoSize = true;
+            this.lblOpcion2.BackColor = System.Drawing.Color.Black;
+            this.lblOpcion2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblOpcion2.Location = new System.Drawing.Point(97, 103);
+            this.lblOpcion2.Name = "lblOpcion2";
+            this.lblOpcion2.Size = new System.Drawing.Size(22, 13);
+            this.lblOpcion2.TabIndex = 1;
+            this.lblOpcion2.Text = "     ";
+            this.lblOpcion2.Click += new System.EventHandler(this.lblOpcion2_Click);
+            // 
+            // lblOpcion3
+            // 
+            this.lblOpcion3.AutoSize = true;
+            this.lblOpcion3.BackColor = System.Drawing.Color.Black;
+            this.lblOpcion3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblOpcion3.Location = new System.Drawing.Point(172, 103);
+            this.lblOpcion3.Name = "lblOpcion3";
+            this.lblOpcion3.Size = new System.Drawing.Size(22, 13);
+            this.lblOpcion3.TabIndex = 2;
+            this.lblOpcion3.Text = "     ";
+            this.lblOpcion3.Click += new System.EventHandler(this.lblOpcion3_Click);
+            // 
+            // btnJugar
+            // 
+            this.btnJugar.Location = new System.Drawing.Point(251, 93);
+            this.btnJugar.Name = "btnJugar";
+            this.btnJugar.Size = new System.Drawing.Size(75, 23);
+            this.btnJugar.TabIndex = 3;
+            this.btnJugar.Text = "Jugar";
+            this.btnJugar.UseVisualStyleBackColor = true;
+            this.btnJugar.Click += new System.EventHandler(this.btnJugar_Click);
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Location = new System.Drawing.Point(20, 171);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(55, 13);
+            this.lbl.TabIndex = 5;
+            this.lbl.Text = "Resultado";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblSeleccionar);
+            this.groupBox1.Controls.Add(this.lblNivel);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtResultado);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnJugar);
+            this.groupBox1.Controls.Add(this.lbl);
+            this.groupBox1.Controls.Add(this.lblOpcion1);
+            this.groupBox1.Controls.Add(this.lblOpcion2);
+            this.groupBox1.Controls.Add(this.lblOpcion3);
+            this.groupBox1.Location = new System.Drawing.Point(25, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(409, 222);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "    ";
+            // 
+            // lblSeleccionar
+            // 
+            this.lblSeleccionar.AutoSize = true;
+            this.lblSeleccionar.Location = new System.Drawing.Point(59, 126);
+            this.lblSeleccionar.Name = "lblSeleccionar";
+            this.lblSeleccionar.Size = new System.Drawing.Size(101, 13);
+            this.lblSeleccionar.TabIndex = 16;
+            this.lblSeleccionar.Text = "Seleccione el orden";
+            this.lblSeleccionar.Visible = false;
+            // 
+            // lblNivel
+            // 
+            this.lblNivel.AutoSize = true;
+            this.lblNivel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNivel.Location = new System.Drawing.Point(333, 11);
+            this.lblNivel.Name = "lblNivel";
+            this.lblNivel.Size = new System.Drawing.Size(16, 16);
+            this.lblNivel.TabIndex = 15;
+            this.lblNivel.Text = "1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(282, 11);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 16);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Nivel";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtResultado.Enabled = false;
+            this.txtResultado.Location = new System.Drawing.Point(91, 164);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.ReadOnly = true;
+            this.txtResultado.Size = new System.Drawing.Size(235, 20);
+            this.txtResultado.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(103, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(178, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(205, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Juego de memoria";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(467, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.empezarJuegoToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // empezarJuegoToolStripMenuItem
+            // 
+            this.empezarJuegoToolStripMenuItem.Name = "empezarJuegoToolStripMenuItem";
+            this.empezarJuegoToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.empezarJuegoToolStripMenuItem.Text = "Empezar juego";
+            this.empezarJuegoToolStripMenuItem.Click += new System.EventHandler(this.empezarJuegoToolStripMenuItem_Click);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(467, 273);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Juego de memoria";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblOpcion1;
+        private System.Windows.Forms.Label lblOpcion2;
+        private System.Windows.Forms.Label lblOpcion3;
+        private System.Windows.Forms.Button btnJugar;
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem empezarJuegoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Label lblNivel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblSeleccionar;
+    }
+}
+
---
+++ AppJuegoAdivina/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppJuegoAdivina
+{
+    public partial class Form1 : Form
+    {
+        List<Int32> listadoOrden = new List<Int32>();
+        List<Int32> listadoOrdenRespondido = new List<Int32>();
+        Boolean revisionResultado = false;
+        Task cambiarColorLabelSeleccionado;
+
+        int cantidadNumeros = 3;
+        int cantidadOpcionesEscogidas = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnJugar_Click(object sender, EventArgs e)
+        {
+            lblSeleccionar.Visible = false;
+            txtResultado.Text = "";
+            generarNumeros();
+            btnJugar.Enabled = false;
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 0; i < listadoOrden.Count; i++)
+            {
+                backgroundWorker1.ReportProgress(listadoOrden[i]);
+                Thread.Sleep(700);
+                backgroundWorker1.ReportProgress(0);
+                Thread.Sleep(700);
+            }
+
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+           switch  (e.ProgressPercentage){
+               case 1:
+                   lblOpcion1.BackColor = Color.Yellow;
+               break;
+
+               case 2:
+               lblOpcion2.BackColor = Color.Yellow;
+               break;
+
+               case 3:
+               lblOpcion3.BackColor = Color.Yellow;
+               break;
+
+               default:
+               lblOpcion1.BackColor = Color.Black;
+               lblOpcion2.BackColor = Color.Black;
+               lblOpcion3.BackColor = Color.Black;
+               break;
+          
+           }
+
+
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            btnJugar.Enabled = true;
+            lblSeleccionar.Visible = true;
+        }
+
+        public void generarNumeros()
+        {
+            Random random = new Random();
+            listadoOrden.Clear();
+
+
+            if (revisionResultado == true)
+            {
+                cantidadNumeros += 1;
+                revisionResultado = false;
+            }
+            
+            for (int i = 0; i < cantidadNumeros; i++)
+            {
+                int randomNumber = random.Next(1, 4);
+                listadoOrden.Add(randomNumber);
+            }
+        }
+
+        private void lblOpcion1_Click(object sender, EventArgs e)
+        {
+            agregarRespuestaRespondida(1);
+             cambiarColorLabelSeleccionado = new Task(() => this.actualizarLabel(1));
+             cambiarColorLabelSeleccionado.Start();
+        }
+
+        private void lblOpcion2_Click(object sender, EventArgs e)
+        {
+            agregarRespuestaRespondida(2);
+             cambiarColorLabelSeleccionado = new Task(() => this.actualizarLabel(2));
+             cambiarColorLabelSeleccionado.Start();
+        }
+
+        private void lblOpcion3_Click(object sender, EventArgs e)
+        {
+            agregarRespuestaRespondida(3);
+             cambiarColorLabelSeleccionado = new Task(() => this.actualizarLabel(3));
+             cambiarColorLabelSeleccionado.Start();
+        }
+
+        public void agregarRespuestaRespondida(int respuesta)
+        {
+            if (cantidadOpcionesEscogidas < cantidadNumeros)
+            {
+                listadoOrdenRespondido.Add(respuesta);
+                cantidadOpcionesEscogidas++;
+            }
+
+            if(cantidadOpcionesEscogidas==cantidadNumeros)
+            {
+                verificarResultado();
+            }
+
+        }
+
+        public void verificarResultado()
+        {
+            //Comparar listadoOrden y listadoOrdenRespondido
+            /*  if ()
+            {
+                revisionResultado = true;
+                txtResultado.Text = "Correcto";
+                lblNivel.Text = (cantidadNumeros - 1)+"";
+            }
+            else
+            {
+                txtResultado.Text = "Incorrecto";
+            }*/
+
+            listadoOrdenRespondido.Clear();
+            cantidadOpcionesEscogidas = 0;
+        }
+
+        private void empezarJuegoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            cantidadNumeros = 3;
+            lblNivel.Text = "1";
+        }
+
+        public void actualizarLabel(int respuesta)
+        {
+
+            switch (respuesta)
+            {
+                case 1:
+            Invoke(new Action(()=>lblOpcion1.BackColor=Color.Blue)); 
+            Thread.Sleep(300);
+                        Invoke(new Action(()=>lblOpcion1.BackColor=Color.Black)); 
+                    break;
+                case 2:
+            Invoke(new Action(()=>lblOpcion2.BackColor=Color.Blue)); 
+            Thread.Sleep(300);
+                        Invoke(new Action(()=>lblOpcion2.BackColor=Color.Black)); 
+                    break;
+
+                case 3:
+           Invoke(new Action(()=>lblOpcion3.BackColor=Color.Blue)); 
+            Thread.Sleep(300);
+                        Invoke(new Action(()=>lblOpcion3.BackColor=Color.Black)); 
+                    break;
+            }
+
+        
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+
+
+    }
+}
